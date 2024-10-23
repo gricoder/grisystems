b4bc88607272aa8e83599b9461fcde5c38d5829d
ikarsu <ibrahim.karsu@forcerta.com>
2024-10-23 15:58:06 +0300

    CopyFolders.ps1 Added

---
+++ FileServer/CopyFolders.ps1
@@ -0,0 +1,46 @@
+﻿#region TAG
+
+### NAME:    Get-AuditFolder.ps1
+### AUTHOR:  ibrahim KARSU  
+### DATE:    20.10.2024
+### WWW:    www.ibrahimkarsu.com
+### TWITTER: @ikarsu
+### LINKEDIN: /ibrahimkarsu
+
+#endregion TAG
+
+
+$sfolder=""
+$dfolder=""
+
+$rcopy="C:\Windows\System32\robocopy.exe"
+$rlpath="C:\Scripts\CopyFolders\robocopy_$(Get-Date -f "hhmm_ddMMyy").txt"
+
+#region Initial Job
+
+&$rcopy $sfolder $dfolder /B /E /COPY:DAT /R:3 /W:5 /MT:64 /UNILOG+:$rlpath /TEE
+
+# /R:3 hata aldığında 3 kez dene
+# /W:5 sn bekle her denemede
+# /COPY:DAT Data, Attribute, Time
+# /E: tüm alt dizinleri, boş olanlar dahil kopyala
+# /B: erişim izni olmayan dosyaları da kopyala
+# /MT:64 adet paralel işlem çalıştır
+# /TEE konsola yazdır
+
+#endregion Initial Job
+
+#region Update Job
+
+&$rcopy $sfolder $dfolder /B /E /COPY:DAT /R:3 /W:5 /MT:64 /XO /UNILOG+:$rlpath /TEE
+
+# /R:3 hata aldığında 3 kez dene
+# /W:5 sn bekle her denemede
+# /COPY:DAT Data, Attribute, Time
+# /E: tüm alt dizinleri, boş olanlar dahil kopyala
+# /B: erişim izni olmayan dosyaları da kopyala
+# /MT:64 adet paralel işlem çalıştır
+# /TEE konsola yazdır
+# /XO yeni dosyaları kopyalar
+
+#endregion Update Job
